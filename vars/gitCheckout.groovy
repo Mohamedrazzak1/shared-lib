5e0f6a92ce9c55a28f30fe3b03acba1e6f8940b2
Mohamed-Razzak <mohd.razack95@gmail.com>
2024-01-30 21:51:48 +0530

    final

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,8 @@
 def call(Map stageParams) {
+ 
     checkout([
         $class: 'GitSCM',
-        branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [[
-            url: stageParams.url,
-            credentialsId: 'pass'
-        ]]
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
