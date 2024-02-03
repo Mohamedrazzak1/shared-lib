9e90f141224c8718a7bb23209216dc014e5c4374
Mohamed-Razzak <mohd.razack95@gmail.com>
2024-02-03 15:51:40 +0530

    docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
  def call(String ImageName, String ImageTag, String DockerHub){
     
      sh """
-      docker image build -t ${AppName}/${ImageName} . 
-      docker image tag ${AppName}/${ImageName} ${AppName}/${ImageName}:${ImageTag}
-      docker image tag ${AppName}/${ImageName} ${AppName}/${ImageName}:latest
+      docker image build -t ${DockerHub}/${ImageName} . 
+      docker image tag ${DockerHub}/${ImageName} ${DockerHub}/${ImageName}:${ImageTag}
+      docker image tag ${DockerHub}/${ImageName} ${DockerHub}/${ImageName}:latest
      """
  }
