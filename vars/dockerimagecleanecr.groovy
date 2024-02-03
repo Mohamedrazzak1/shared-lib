68d12d3a799621458e89d1b20e4a46d79b3b4f28
Mohamed-Razzak <mohd.razack95@gmail.com>
2024-02-03 22:58:38 +0530

    22

--- vars/dockerimagecleanecr.groovy
+++ vars/dockerimagecleanecr.groovy
@@ -1,6 +1,8 @@
 def call(String aws_account_id, String Region, String ECR_REPO_NAME){
     
     sh """
-     docker rmi ${aws_account_id}:latest ${aws_account_id}.dkr.ecr.${Region}.amazonaws.com/${ECR_REPO_NAME}:latest
+     docker rmi ${aws_account_id}.dkr.ecr.${Region}.amazonaws.com/${ECR_REPO_NAME}:latest
     """
 }
+
+
