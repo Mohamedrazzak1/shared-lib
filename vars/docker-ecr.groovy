3ec3f600abe082c68d5d8b519190989aeef1c5ef
Mohamed-Razzak <mohd.razack95@gmail.com>
2024-02-03 22:29:29 +0530

    ll

--- vars/docker-ecr.groovy
+++
@@ -1,9 +0,0 @@
-def call(String aws_account_id, String Region, String ECR_REPO_NAME){
-    
-    sh """
-     docker build -t ${ECR_REPO_NAME} .
-     docker tag ${ECR_REPO_NAME}:latest ${aws_account_id}.dkr.ecr.${Region}.amazonaws.com/${ECR_REPO_NAME}:latest
-    """
-}
-
-
